0b5051bd7e6d3d234d31bf0b79f0d676cfd39d6f
FruitySyrup <106176876+FruitySyrup@users.noreply.github.com>
2022-07-28 15:10:08 -0400

    edit readme and fixed bug

--- app.js
+++ app.js
@@ -21,6 +21,6 @@ form.addEventListener('submit', (e) => {
 });
 
 function validateEmail (email) {
-	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	return re.test(String(email).toLowerCase());
+	const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
 }
